81d249b2bbd4c2cc1bdd8dc85e613332444341d9
Jake Rahu <jake_rahu@hotmail.com>
2024-06-13 16:14:07 +0300

    initial commit

--- apigateway.tf
+++ apigateway.tf
@@ -35,27 +35,6 @@ resource "aws_apigatewayv2_stage" "apigateway" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "lambda" {
-  for_each           = local.routes
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_uri    = aws_lambda_function.lambda[each.value.name].invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "default_route" {
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda["index"].id}"
-}
-
-resource "aws_apigatewayv2_route" "lambda" {
-  for_each  = local.routes
-  api_id    = aws_apigatewayv2_api.api.id
-  route_key = "${each.value.http_verb} ${each.value.path}"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda[each.value.name].id}"
-}
-
 resource "aws_cloudwatch_log_group" "api_gw" {
   name              = "/aws/api_gw/${aws_apigatewayv2_api.api.name}"
   retention_in_days = 1
