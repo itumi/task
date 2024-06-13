75edd6a9e6587b1ec6e2ecda4f2c20c0c5afa51f
Jake Rahu <jake_rahu@hotmail.com>
2024-06-13 15:41:29 +0300

    initial commit

--- variables.tf
+++ variables.tf
@@ -21,34 +21,6 @@ locals {
       policies : "logs:List*",
       resource : "arn:aws:logs:*:*:*"
 
-    },
-    "questions" : {
-      name : "questions"
-      http_verb : "GET"
-      path = "/questions"
-      policies : ["dynamodb:Scan"]
-      resource : [aws_dynamodb_table.questions.arn]
-      environment : {
-        variables = {
-          jwtSecret = var.jwtSecret
-        }
-      }
-    },
-    "question-post" : {
-      name : "question-post"
-      http_verb : "POST"
-      path = "/question"
-      policies : ["dynamodb:PutItem", "dynamodb:Scan"]
-      resource : [aws_dynamodb_table.questions.arn]
-    },
-    "question-delete" : {
-      name : "question-delete"
-      http_verb : "DELETE"
-      path = "/question"
-      policies : ["dynamodb:DeleteItem"]
-      resource : [aws_dynamodb_table.questions.arn]
-
-    },
+    }
   }
 }
-
