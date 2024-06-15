bfa143dc3c8f8e7dc64e438151f2f131ad7dd9f0
Jake Rahu <jake_rahu@hotmail.com>
2024-06-15 12:29:03 +0300

    initial commit

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "la-pa"
+    region = "us-west-2"
+    key    = "terraform.tfstate"
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
@@ -6,6 +20,22 @@ provider "aws" {
 resource "random_string" "suffix" {
   length  = 8
   special = false
+}
+
+# Data source to get the latest Amazon Linux 2 AMI
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 # VPC
