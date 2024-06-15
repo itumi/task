a67b7de824be21468770e56a845f6af0b81fbce5
Jake Rahu <jake_rahu@hotmail.com>
2024-06-15 12:11:21 +0300

    initial commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "la-pa" 
+    bucket = "la-pa"
     region = "us-west-2"
     key    = "terraform.tfstate"
   }
@@ -52,6 +52,65 @@ resource "aws_security_group" "main" {
   }
 }
 
+# Load Balancer
+resource "aws_lb" "app" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main.id]
+  subnets            = aws_subnet.main[*].id
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_target_group" "app" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path     = "/"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+
+# Auto Scaling Group
+resource "aws_launch_configuration" "app" {
+  name          = "app-launch-config"
+  image_id      = "ami-0c55b159cbfafe1f0"  # Update to your AMI ID
+  instance_type = var.instance_type
+
+  security_groups = [aws_security_group.main.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "app" {
+  desired_capacity          = 2
+  max_size                  = 3
+  min_size                  = 1
+  vpc_zone_identifier       = aws_subnet.main[*].id
+  target_group_arns         = [aws_lb_target_group.app.arn]
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+
+  launch_configuration = aws_launch_configuration.app.id
+}
+
 # Outputs
 output "vpc_id" {
   value = aws_vpc.main.id
@@ -63,4 +122,8 @@ output "subnet_ids" {
 
 output "security_group_id" {
   value = aws_security_group.main.id
+}
+
+output "load_balancer_dns_name" {
+  value = aws_lb.app.dns_name
 }
