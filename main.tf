29b2948d4ee089d19b093cf33e6c7052f841d817
Jake Rahu <jake_rahu@hotmail.com>
2024-06-15 12:14:17 +0300

    initial commit

--- main.tf
+++ main.tf
@@ -21,11 +21,34 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 }
 
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
 # Subnets
 resource "aws_subnet" "main" {
   count = length(var.subnets)
   vpc_id = aws_vpc.main.id
   cidr_block = element(var.subnets, count.index)
+  map_public_ip_on_launch = true
+}
+
+# Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "a" {
+  count = length(var.subnets)
+  subnet_id = element(aws_subnet.main[*].id, count.index)
+  route_table_id = aws_route_table.main.id
 }
 
 # Security Group
