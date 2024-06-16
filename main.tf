37dafd595800ac177b201df5c850a6a9838368ba
Jake Rahu <jake_rahu@hotmail.com>
2024-06-16 15:28:38 +0300

    Initial commit

--- main.tf
+++ main.tf
@@ -147,6 +147,16 @@ resource "aws_launch_template" "app" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y nginx awscli
+              aws s3 cp s3://${var.aws_s3_bucket}/react_build.zip /tmp/react_build.zip
+              sudo apt-get install -y unzip
+              unzip /tmp/react_build.zip -d /usr/share/nginx/html/
+              sudo service nginx start
+              EOF
+
   tag_specifications {
     resource_type = "instance"
     tags = {
